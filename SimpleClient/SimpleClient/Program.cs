f2242b7d05a80a7b233d3b6f1f69ba3f5a19eccd
Scott Walton <scottmwalton@hotmail.com>
2015-02-10 20:49:00 -0600

    Initial project version

---
+++ SimpleClient/SimpleClient/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SimpleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+			try
+			{
+				IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9097);
+				//IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("172.16.1.4"), 9097);
+
+	            client.Connect(serverEndPoint);
+
+	            NetworkStream clientStream = client.GetStream();
+
+	            ASCIIEncoding encoder = new ASCIIEncoding();
+	            byte[] buffer = encoder.GetBytes("Hello Server!");
+	            byte[] message = new byte[4096];
+	            int bytesRead = 0;
+				int i = 0;
+				while(i<10)
+				{
+		            clientStream.Write(buffer, 0, buffer.Length);
+		            clientStream.Flush();
+		            try
+		            {
+		                //blocks until a client sends a message
+		                bytesRead = clientStream.Read(message, 0, 4096);
+		            }
+		            catch(Exception ex)
+		            {
+		                //a socket error has occurred
+		                Console.WriteLine("ERROR: {0}\n{1}", ex.Message, ex.ToString());
+		                return;
+		            }
+
+		            if (bytesRead == 0)
+		            {
+		                //the server was disconnected
+		                Console.WriteLine("Server disconnect. :(");
+		            }
+
+		            //message has successfully been received
+		            Console.WriteLine(encoder.GetString(message, 0, bytesRead));
+					i++;
+					Thread.Sleep(5000);
+				}
+			}
+			catch(Exception ex) 
+			{
+				Console.WriteLine ("ERROR: {0}\n{1}", ex.Message, ex.ToString ());
+			}
+			finally 
+			{
+				client.Close ();
+			}
+        }
+    }
+}
---
+++ SimpleServer/SimpleServer/Server.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace SimpleServer
+{
+    public class Server
+    {
+        private TcpListener _tcpListener;
+        private Thread _listenThread;
+
+        public Server()
+        {
+            this._tcpListener = new TcpListener(IPAddress.Any, 9097);
+            this._listenThread = new Thread(new ThreadStart(ListenForClients));
+            this._listenThread.Start();
+        }
+
+        private void ListenForClients()
+        {
+            this._tcpListener.Start();
+
+            while(true)
+            {
+                // blocks until a client has connected to the server
+                TcpClient client = this._tcpListener.AcceptTcpClient();
+
+                // create a thread to handle communication with connected client
+                Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClientComm));
+                clientThread.Start(client);
+            }
+        }
+
+        private void HandleClientComm(object client)
+        {
+            TcpClient tcpClient = (TcpClient)client;
+            NetworkStream clientStream = tcpClient.GetStream();
+
+            byte[] message = new byte[4096];
+            int bytesRead;
+
+            while(true)
+            {
+                bytesRead = 0;
+
+                try
+                {
+                    // blocks until a client sends a message
+                    bytesRead = clientStream.Read(message, 0, 4096);
+                }
+                catch(Exception ex)
+                {
+                    // a socket error has occurred
+                    Console.WriteLine("ERROR: {0}\n{1}", ex.Message, ex.ToString());
+                    break;
+                }
+
+                if(bytesRead == 0)
+                {
+                    // the client has disconnected from the server
+                    break;
+                }
+
+                // message has successfully been received
+                ASCIIEncoding encoder = new ASCIIEncoding();
+                Console.WriteLine("RCV MSG:{0}", encoder.GetString(message, 0, bytesRead));
+
+                byte[] buffer = encoder.GetBytes("Hello Client!");
+
+                clientStream.Write(buffer, 0, buffer.Length);
+                clientStream.Flush();
+            }
+
+            tcpClient.Close();
+        }
+    }
+}
